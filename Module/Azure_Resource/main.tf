c244b36b5f59f1331e8b4019411053880be7d35a
shubhamsinghvi8696 <sshubham8696@gmail.com>
2025-03-23 10:14:12 +0530

    Added Location on Resource block

--- Module/Azure_Resource/main.tf
+++ Module/Azure_Resource/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "resource_Practice" {
   for_each   = var.RG_Infra
   name       = each.value.name
-  location   = each.value
+  location   = each.value.location
   tags       = each.value.tags
   managed_by = each.value.managed_by
 }
